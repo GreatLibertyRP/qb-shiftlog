8d7ba29d1ab0fb7e02d7a4479a11e2c79504c92b
Bama94 <tegober94@hotmail.com>
2022-03-01 14:03:08 -0600

    edited config

--- config/sh_config.lua
+++ config/sh_config.lua
@@ -1,7 +1,7 @@
 -- Wowow an epic shared file
 Config = {} -- We must initialise the config table to be able to do config.anything
 
-Config.policeWebhook=""
-Config.ambulanceWebhook=""
-Config.mechanicWebhook=""
+Config.policeWebhook="https://discord.com/api/webhooks/948062437666611242/ZlexLYYLt6Yzc37mZ8pY_JTgbNCuqDqfUPWq6eks1jmRfG5eFE2Qa_ZC4ASWW9_wwR6x"
+Config.ambulanceWebhook="https://discord.com/api/webhooks/948062547871940639/Y6neWRRaI-xQ0fwNNSywDX906mbOw5gmSwgDvVkMH2s7R-R3c5Zxekk0JDV2AmNAIKae"
+Config.mechanicWebhook="https://discord.com/api/webhooks/948062648703021076/6N-kmZIMI7pEw1lnZuzFNJBHF1hpd-2yR6zMqtuE1W_QmDPzUdb3i60WV8euYgcjpKDE"
 
